7850e6308d5fd9c4cdf44b13c3c5130308480e33
Sahil Shaikh <62303954+sahil1409@users.noreply.github.com>
2022-08-09 15:50:48 +0530

    My First WinForm

---
+++ WinForms/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class b : Form
+    {
+        public b()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClickThis_Click(object sender, EventArgs e)
+        {
+            lblHelloWorld.Text = "Hello World";
+        }
+    }
+}
